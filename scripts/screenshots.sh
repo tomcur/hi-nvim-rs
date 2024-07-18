c8658e41068abaf67f84b4fd49f8c18dccb2e973
Thomas Churchman <thomas@kepow.org>
2024-07-18 17:17:39 +0200

    chore: regenerate examples

--- scripts/screenshots.sh
+++ scripts/screenshots.sh
@@ -15,13 +15,16 @@ for light_dark in "light" "dark"; do
     for colorscheme in "highlow" "twocolor" "grayscale"; do
         cargo run -- ./colorschemes/$colorscheme.toml >"$DIR/$colorscheme.vim"
 
-        termsnap --term xterm-256color --columns 58 --lines 20 --render-before-clear -- \
+        (
+            # exit on a timer to perform some other commands in the meantime
+            echo ":lua vim.defer_fn(function() vim.cmd('q!') end, 50)"
+            echo -ne ":12\rnVjj$"
+            sleep 0.1
+        ) | termsnap --term xterm-256color --columns 58 --lines 20 --render-before-clear -- \
             "$NVIM" --clean \
             -c "source ./scripts/nvim-config.vim" \
             -c "set bg=$light_dark" \
             -c "source $DIR/$colorscheme.vim" \
-            `# defer to ensure neovim tui has settled` \
-            -c "lua vim.defer_fn(function() vim.cmd('q') end, 0)" \
             ./hi-nvim-rs-web/code-examples/rust.rs \
             >./media/$colorscheme-$light_dark.svg
     done
