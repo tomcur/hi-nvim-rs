0d017034c43108d96795690bc506c83affc395be
Thomas Churchman <thomas@kepow.org>
2024-07-19 10:04:51 +0200

    docs: improve screenshot generation script

--- scripts/screenshots.sh
+++ scripts/screenshots.sh
@@ -11,21 +11,26 @@ if [ -n "$SCREENSHOT_NVIM" ]; then
     NVIM="$SCREENSHOT_NVIM"
 fi
 
+cargo build --release
+BIN=./target/release/hi-nvim-rs
+
 for light_dark in "light" "dark"; do
     for colorscheme in "highlow" "verf" "twocolor" "grayscale"; do
-        cargo run -- ./colorschemes/$colorscheme.toml >"$DIR/$colorscheme.vim"
+        $BIN -- ./colorschemes/$colorscheme.toml >"$DIR/$colorscheme.vim"
 
         (
             # exit on a timer to perform some other commands in the meantime
             echo ":lua vim.defer_fn(function() vim.cmd('q!') end, 50)"
             echo -ne ":12\rnVjj$"
-            sleep 0.1
+            sleep 0.5
         ) | termsnap --term xterm-256color --columns 58 --lines 20 --render-before-clear -- \
             "$NVIM" --clean \
             -c "source ./scripts/nvim-config.vim" \
             -c "set bg=$light_dark" \
             -c "source $DIR/$colorscheme.vim" \
             ./hi-nvim-rs-web/code-examples/rust.rs \
-            >./media/$colorscheme-$light_dark.svg
+            >./media/$colorscheme-$light_dark.svg &
     done
 done
+
+wait
